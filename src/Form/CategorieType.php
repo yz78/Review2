5c7b590bfd96da8ce2c561765425e162b6857ad7
fayeza78 <fayeza.tibedeo@gmail.com>
2024-12-04 11:04:55 +0100

    Catégorie style

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Categorie;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,12 @@ class CategorieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle',)
-            ->add('videos')
+            ->add('libelle', TextType::class, [
+                'label' => 'Libellé',
+                'attr' => [
+                    'placeholder' => 'Entrez le libellé de la catégorie',
+                ],
+            ])
         ;
     }
 
