2c9bd63522fc3e8e41d1a164c6bae0f08e71314d
fayeza78 <fayeza.tibedeo@gmail.com>
2024-12-12 12:08:10 +0100

    changer de mot de passe

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre mot de passe actuel']),
+                ],
+            ])
+            ->add('newPassword', PasswordType::class, [
+                'label' => 'Nouveau mot de passe',
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un nouveau mot de passe']),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('confirmPassword', PasswordType::class, [
+                'label' => 'Confirmez le mot de passe',
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez confirmer votre mot de passe']),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
