7702c073c22b2f8d59bf04ce66dac3af7c600b1c
fayeza78 <fayeza.tibedeo@gmail.com>
2024-12-18 14:31:31 +0100

    page profil

--- src/Controller/Createurcontenu/ProfilController.php
+++ src/Controller/Createurcontenu/ProfilController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller\Createurcontenu;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use id;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
 
 class ProfilController extends AbstractController
 {
-    #[Route('/createurcontenu/profil', name: 'app_createurcontenu_profil')]
-    public function index(): Response
+    #[Route('/createur-contenu/profil/{id}', name: 'app_createur_contenu_profil')]
+    #[IsGranted('ROLE_USER')] // Protéger l'accès à la route
+    public function index(int $id): Response
     {
+        $user = $this->getUser(); // Récupère l'utilisateur connecté
+
+        if (!$user || $user->getId() !== $id) {
+            throw $this->createAccessDeniedException('Accès interdit.');
+        }
+
         return $this->render('createurcontenu/profil.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user,
         ]);
     }
 }
